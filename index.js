7d751be0428c5e7f264c620b959e33d2fe5beee9
Antone <email@antone.dev>
2022-08-21 21:20:44 -0700

    testing

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const puppeteer = require("puppeteer");
 const express = require("express");
+const puppeteer = require("puppeteer");
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ response: "B-OK" });
 })
 
-app.get("/bex/create", (req, res) => {
-    let firstName = req.query["firstName"];
-    let lastName = req.query["lastName"];
+app.post("/bex/create-agent", (req, res) => {
+    let firstName = req.body["firstName"];
+    let lastName = req.body["lastName"];
     if (!firstName || !lastName) return res.json({ error: "First and last name must be provided." });
     res.json({ firstName: firstName, lastName: lastName });
 })
 
 app.listen(9000);
-
-//testing
