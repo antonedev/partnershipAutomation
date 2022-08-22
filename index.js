79e43ce8e4841852eeaa5310ea9675c38702db89
Antone <email@antone.dev>
2022-08-21 19:21:33 -0700

    testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ response: "A-OK" });
+    res.json({ response: "B-OK" });
 })
 
 app.listen(9000, (e) => {
