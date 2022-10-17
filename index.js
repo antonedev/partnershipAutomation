686c87d5e39c9e72f781d05766c1ad236def595b
Antone <email@antone.dev>
2022-10-17 14:42:02 -0700

    testing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const nodemailer = require("nodemailer");
 const express = require("express");
 const puppeteer = require("puppeteer");
 
@@ -14,7 +15,63 @@ app.post("/bex/create-agent", (req, res) => {
     let firstName = req.body["firstName"];
     let lastName = req.body["lastName"];
     if (!firstName || !lastName) return res.status(400).json({ Error: "firstName and lastName are required." });
-    res.status(202).json({ firstName: firstName, lastName: lastName });
+    res.status(202).json({ firstName: firstName, lastName: lastName, AuthCode: req.headers.authorization });
+    return;
+    (async (firstName, lastName) => {
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto("https://app.mailparser.io/account/login", {
+            waitUntil: 'domcontentloaded'
+        });
+        await page.type("#email", process.env.MP_ACCT);
+        await page.type("#password", process.env.MP_PASS);
+        await page.click("#start-free-sub");
+        await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+        await page.click("#dashboard_inbox_add");
+        await page.type("input[name='label']", `${fir}`);
+        await page.select("select[name='inbox_category_id']", "3015");
+        await page.click("#btn_add_address_save");
+        await page.waitForNavigation({ waitUntil: "load" });
+        const address = await page.$eval("a.created_inbox_link", anchor => anchor.textContent);
+        const transporter = await nodemailer.createTransport({
+            host: "mx.antone.dev",
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.MX_ACCT,
+                pass: process.env.MX_PASS,
+            },
+        });
+        await transporter.sendMail({
+            from: "'Automation Testing' <automation@antone.dev>",
+            to: address,
+            subject: "Automation Testing",
+            text: `First Name: Test
+            Last Name: Person
+            Type: Purchase
+            Property: https://www.bexrealty.com/Arizona/Tucson/133-E-Horizon-Cir/condominium/
+            Property Address: 123 TEST CIR
+            City: TUCSON
+            State: AZ
+            Zip: 85737
+            MLS: 1234567
+            Phone: (858) 123-4567
+            Email: testlead@test.com
+            Development: Horizon Heights Condominiums
+            Price: $299,900
+            Source: BEX Realty
+            Partner Agent: Test Man`,
+        });
+        await page.waitForSelector("#done_wrapper", {
+            visible: true,
+        });
+        await page.click("#done_wrapper > div > a");
+        await page.waitForSelector("#wizard_save", { visible: true });
+        await page.click("#wizard_save");
+        await page.waitForNavigation({waitUntil: "domcontentloaded"});
+        await browser.close();
+    })();
 })
 
 app.listen(9000);
