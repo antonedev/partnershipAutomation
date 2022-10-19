92f1e04e752c982bbe4b6fc17147a5bf11c8d3e5
Antone <email@antone.dev>
2022-10-19 11:50:42 -0700

    testing

--- index.js
+++ index.js
@@ -21,23 +21,28 @@ app.post("/bex/create-agent", (req, res) => {
     let firstName = req.body["firstName"];
     let lastName = req.body["lastName"];
     res.status(202).json({ firstName: firstName, lastName: lastName });
-    console.log(`New Request: ${firstName} ${lastName}`);
+    console.log(`New Request: ${firstName} ${lastName}\nNotification Email: ${notificationEmail}`);
+
     (async (firstName, lastName) => {
         const browser = await puppeteer.launch({ executablePath: "/usr/bin/chromium", headless: true });
         const page = await browser.newPage();
+        console.log(`Browser opened`);
         await page.setDefaultNavigationTimeout(0);
         await page.goto("https://app.mailparser.io/account/login", {
             waitUntil: 'domcontentloaded'
         });
+        console.log(`Loaded ${page.url()}`);
         await page.type("#email", process.env.MP_ACCT);
         await page.type("#password", process.env.MP_PASS);
         await page.click("#start-free-sub");
         await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+        console.log(`Loaded ${page.url()}`);
         await page.click("#dashboard_inbox_add");
         await page.type("input[name='label']", `${firstName} ${lastName}`);
         await page.select("select[name='inbox_category_id']", "3015");
         await page.click("#btn_add_address_save");
         await page.waitForNavigation({ waitUntil: "load" });
+        console.log(`Loaded ${page.url()}`);
         const address = await page.$eval("a.created_inbox_link", anchor => anchor.textContent);
         const transporter = await nodemailer.createTransport({
             host: process.env.MX_HOST,
@@ -73,8 +78,10 @@ app.post("/bex/create-agent", (req, res) => {
         });
         await page.click("#done_wrapper > div > a");
         await page.waitForSelector("#wizard_save", { visible: true });
+        console.log(`Loaded ${page.url()}`);
         await page.click("#wizard_save");
         await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+        console.log(`Loaded ${page.url()}`);
         const inboxURL = page.url();
         const notificationTransporter = await nodemailer.createTransport({
             host: process.env.MX_HOST,
@@ -94,6 +101,7 @@ app.post("/bex/create-agent", (req, res) => {
             Mailparser Address: ${address}`,
         });
         await browser.close();
+        console.log(`Browser closed`);
     })();
 })
 
